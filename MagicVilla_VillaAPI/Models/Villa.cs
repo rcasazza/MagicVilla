666b2110a29edd871f44620440c558e60c30b49d
Robert Casazza <rjc@oasis.local>
2023-05-25 19:01:03 -0400

    first endpoint

---
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace MagicVilla_VillaAPI.Models
+{
+	public class Villa
+	{
+        public int Id { get; set; }
+        public String Name { get; set; }
+
+        public Villa()
+		{
+
+		}
+	}
+}
+
